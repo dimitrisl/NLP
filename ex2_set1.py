bc2b3ffdd3182a814d4db23848ea5ad9c4e67615
Dimitrisl <dlolis88@gmail.com>
2017-03-16 20:50:14 +0200

    fixed levenstein

--- ex2_set1.py
+++ ex2_set1.py
@@ -16,22 +16,17 @@ def count_distance(lettertarget,lettersource,listoflists,x,y):
         cost = min(listoflists[x-1][y]+1,listoflists[x][y-1]+1,listoflists[x-1][y-1]+2)
         return cost
 
-
 word1 = "paizete"  # target string
 word2 = "pezoitai" # source string
 listoflists = [] # "array" that contains the distances
-
 listoflists = [[i for i in range(len(word1)+1)]] # initialization of the first vector,vectors are lines!
+
 for i in range(1,len(word2)+1):
     listoflists.append([i])
-    listoflists[-1].extend([0 for k in range(len(word2)-1)])
+    listoflists[-1].extend([0 for k in range(len(word1))])
 
-
-#we wont iterate line 0 at all!!
-
-for w1,x in zip(word2,range(1,len(word2)+1)):
-    for w2,y in zip(word1,range(1,len(word1)+1)):
+for x,w2 in enumerate(word2,1):
+    for y,w1 in enumerate(word1,1):
         listoflists[x][y] = count_distance(w1,w2,listoflists,x,y)
-
 for i in listoflists:
     print i
