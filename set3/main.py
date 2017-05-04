a7031593accea190fc051af0e987c371b60c6b69
vasiapars <vasiliki.chira@gmail.com>
2017-05-05 02:52:03 +0300

    viterbi decoder

--- set3/main.py
+++ set3/main.py
@@ -2,18 +2,20 @@ from spacy.en import English
 from nltk.tokenize import sent_tokenize
 from collections import Counter
 from languagemodel import *
-from spelling_corrector import levenstein
+from spelling_corrector import viterbidecoder
 
 
 nlp = English()
 with open('europarl.txt', 'r') as f:
     content = f.read()
 
-sentences = [sent for sent in sent_tokenize(content[0:200000].decode('utf-8'))]
+sentences = ['#start0 #start1 '+sent for sent in sent_tokenize(content[0:200000].decode('utf-8'))]
 words = [str(token) for sent in sentences for token in nlp(sent, 'utf-8')]
 lexicon = Counter(words)
 lexicon = find_rare(lexicon)
-logprob_bigrams={}
+logprob_bigrams = {}
 bigrams, logprob_bigrams = lpbigrams(words, lexicon)
-
-print levenstein('the',lexicon)
+given_sentence = "#start0 #start1 You hve requstd a debte on ths sject in the crse of the nxt few days, dng this pt-session."
+print ("The sentence given was"+given_sentence)
+correct, b = viterbidecoder(given_sentence, lexicon, logprob_bigrams)
+print ("THe sentence returned is " + correct)
--- set3/spelling_corrector.py
+++ set3/spelling_corrector.py
@@ -1,16 +1,32 @@
 from ldistance import get_dist
+from math import log
+from nltk.tokenize import word_tokenize
 
 
 def levenstein(word, lexicon):
     ldistance = {}
     for i in lexicon.keys():
-        ldistance[(word, i)] = get_dist(word, i)
+        ldistance[i] = get_dist(word, i)
     min_val = min(ldistance.itervalues())
-    correct_word = [(words, k) for (words, k) in ldistance.iteritems() if k == min_val]
-    return correct_word
+    correct_word = [(word, k) for word, k in ldistance.iteritems() if k == min_val]
+    return correct_word[0]
 
 
-def viterbidecoder():
-    pass
+def viterbidecoder(sentence, lexicon, logprob_bigrams):
+    V = {}
+    tokens = [token for token in word_tokenize(sentence)]
+    word, dist = levenstein(tokens[2], lexicon)
+    correct_sequence = ['#start0','#start1']
+    correct_sequence.append(word)
+    V[0] = 0
+    V[1] = log(1/float(dist + 1.1)) + logprob_bigrams[(tokens[0], tokens[1])]
+    for i in range(2, len(tokens)+1):
+        next_word, dist = levenstein(tokens[i+1], lexicon)
+        correct_sequence.append(next_word)
+        if (correct_sequence[i-1], correct_sequence[i]) in logprob_bigrams.keys():
+            V[i] = log(1 / float(dist + 1.1)) + logprob_bigrams[(correct_sequence[i-1], correct_sequence[i])] + V[i-1]
+        else:
+            V[i] = log(1 / float(dist + 1.1)) + 1/float(len(lexicon)) + V[i - 1]
+    return correct_sequence, V
 
 
