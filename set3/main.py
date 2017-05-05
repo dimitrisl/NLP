13a4d2322e1ae2b4125bfff97f10d8c6a12746f0
vasiapars <vasiliki.chira@gmail.com>
2017-05-05 12:47:42 +0300

    viterbi decoder one sequence

--- set3/main.py
+++ set3/main.py
@@ -9,13 +9,14 @@ nlp = English()
 with open('europarl.txt', 'r') as f:
     content = f.read()
 
-sentences = ['#start0 #start1 '+sent for sent in sent_tokenize(content[0:200000].decode('utf-8'))]
+sentences = [sent for sent in sent_tokenize(content[0:200000].decode('utf-8'))]
 words = [str(token) for sent in sentences for token in nlp(sent, 'utf-8')]
 lexicon = Counter(words)
 lexicon = find_rare(lexicon)
 logprob_bigrams = {}
 bigrams, logprob_bigrams = lpbigrams(words, lexicon)
-given_sentence = "#start0 #start1 You hve requstd a debte on ths sject in the crse of the nxt few days, dng this pt-session."
+given_sentence = "You hve requstd a debte on ths sject in the crse of the nxt few days, dng this pt-session."
 print ("The sentence given was"+given_sentence)
 correct, b = viterbidecoder(given_sentence, lexicon, logprob_bigrams)
-print ("THe sentence returned is " + correct)
+print correct
+print b
--- set3/spelling_corrector.py
+++ set3/spelling_corrector.py
@@ -15,13 +15,14 @@ def levenstein(word, lexicon):
 def viterbidecoder(sentence, lexicon, logprob_bigrams):
     V = {}
     tokens = [token for token in word_tokenize(sentence)]
-    word, dist = levenstein(tokens[2], lexicon)
-    correct_sequence = ['#start0','#start1']
+    word, dist = levenstein(tokens[0], lexicon)
+    correct_sequence = ['#start0', "#start1"]
+    print word
     correct_sequence.append(word)
     V[0] = 0
-    V[1] = log(1/float(dist + 1.1)) + logprob_bigrams[(tokens[0], tokens[1])]
-    for i in range(2, len(tokens)+1):
-        next_word, dist = levenstein(tokens[i+1], lexicon)
+    V[1] = log(1/float(dist + 1.1)) + logprob_bigrams[(correct_sequence[0], correct_sequence[1])]
+    for i in range(2, len(tokens)):
+        next_word, dist = levenstein(tokens[i-1], lexicon)
         correct_sequence.append(next_word)
         if (correct_sequence[i-1], correct_sequence[i]) in logprob_bigrams.keys():
             V[i] = log(1 / float(dist + 1.1)) + logprob_bigrams[(correct_sequence[i-1], correct_sequence[i])] + V[i-1]
