98ac724d722ce577d4add742b27b7d24c5adfffb
dimitrisl <dlolis88@gmail.com>
2017-05-25 22:51:11 +0300

    untested

--- set5/main.py
+++ set5/main.py
@@ -1,7 +1,32 @@
 import numpy as np
 
-def check_grammar(word):
-    pass
+
+def check_grammar(turn, word, ternary, non_ternary):
+    if turn == "first":
+        for line in ternary:
+            if line[1] == word:
+                return line[0]#  the type!
+    else:
+        states = word.split(" ")
+        for symbol1, symbol2 in non_ternary:
+            if (states[0],states[1]) == (symbol1,symbol2) or (states[0],states[1]) == (symbol2,symbol1):
+                return symbol1
+    return states
+
+
+def grammar():
+    f = open("grammar")
+    rules = f.readlines()
+    rules = [line.replace(" -> ", " ").split(" ") for line in rules]
+    list1 = []
+    list2 = []
+    for line in rules:
+        if len(line) == 2:
+            list1.append(line)
+        else:
+            list2.append(line)
+
+    return list1, list2
 
 sentence = "I saw the man with the telescope"
 words = sentence.split(" ")
@@ -10,15 +35,18 @@ collumns = len(words)+1
 matrix = np.chararray((rows, collumns), itemsize=5)
 matrix[:] = "" #initialize
 
+ternary, non_ternary = grammar()
+
 for j in range(collumns):
     for i in range(rows):
         if j > i:
+            status = ""
             if i+1 == j:
-                matrix[i, j] = "match" #first match
+                matrix[i, j] = check_grammar("first", words[i], ternary, non_ternary)#check_grammar(words[i]) #first match
                 #check_grammar(words[0])
             else:
                 for k in range(1, j):
                     con = matrix[i, k] + " " + matrix[k, j] #concatenation of the two states
-                    status = check_grammar(con)
+                    status += check_grammar("second", con, ternary, non_ternary)
                     matrix[i, j] = status
 print matrix
