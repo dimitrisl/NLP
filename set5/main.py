87f1dc84c69487676e7fcd9c37bd277451dbb8b1
dimitrisl <dlolis88@gmail.com>
2017-05-26 12:44:02 +0300

    basic trouble

--- set5/main.py
+++ set5/main.py
@@ -8,11 +8,12 @@ def check_grammar(turn, word, ternary, non_ternary):
                 return line[0]#  the type!
     else:
         states = word.split(" ")
+        print states
         for symbol1, symbol2, symbol3 in non_ternary:
-            if (states[0], states[1]) == (symbol2,symbol3) or (states[0], states[1]) == (symbol3, symbol2):
+            if (states[0], states[1]) == (symbol2, symbol3) or (states[0], states[1]) == (symbol3, symbol2):
                 return symbol1
-    return states
-
+            else:
+                return ""
 
 def grammar():
     f = open("grammar")
@@ -21,6 +22,7 @@ def grammar():
     list1 = []
     list2 = []
     for line in rules:
+        line = [i.strip() for i in line]
         if len(line) == 2:
             list1.append(line)
         else:
@@ -33,20 +35,18 @@ words = sentence.split(" ")
 rows = len(words)
 collumns = len(words)+1
 matrix = np.chararray((rows, collumns), itemsize=5)
-matrix[:] = "" #initialize
+matrix[:] = " " #initialize
 
 ternary, non_ternary = grammar()
 
 for j in range(collumns):
     for i in range(rows):
         if j > i:
-            status = ""
             if i+1 == j:
                 matrix[i, j] = check_grammar("first", words[i], ternary, non_ternary)#check_grammar(words[i]) #first match
                 #check_grammar(words[0])
             else:
                 for k in range(1, j):
-                    con = matrix[i, k] + " " + matrix[k, j] #concatenation of the two states
-                    status += check_grammar("second", con, ternary, non_ternary)
-                    matrix[i, j] = status
+                    con = matrix[i, k] + " " + matrix[k, j-1] #concatenation of the two states
+                    matrix[i, j] += check_grammar("second", con, ternary, non_ternary)
 print matrix
