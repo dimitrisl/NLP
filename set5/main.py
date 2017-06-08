6058dc1c2ad5996046a3ea6a8f8932258e2689b2
Dimitrisl <dlolis88@gmail.com>
2017-06-08 23:13:03 +0300

    grammar ready

--- set5/main.py
+++ set5/main.py
@@ -7,13 +7,16 @@ def check_grammar(turn, word, ternary, non_ternary):
             if line[1] == word:
                 return line[0]#  the type!
     else:
-        states = word.split(" ")
-        print states
-        for symbol1, symbol2, symbol3 in non_ternary:
-            if (states[0], states[1]) == (symbol2, symbol3) or (states[0], states[1]) == (symbol3, symbol2):
-                return symbol1
-            else:
-                return ""
+        states = word.split(",")
+        left = states[0]
+        print "left",left
+        print "right",states[1]
+        right = states[1]
+        goals = []
+        for line in non_ternary:
+            if left == line[1] and right == line[2]:
+                goals.append(line[0])
+        return ','.join(goals)
 
 def grammar():
     f = open("grammar")
@@ -34,19 +37,36 @@ sentence = "I saw the man with the telescope"
 words = sentence.split(" ")
 rows = len(words)
 collumns = len(words)+1
-matrix = np.chararray((rows, collumns), itemsize=5)
-matrix[:] = " " #initialize
+
+matrix = [["" for j in range(collumns)] for i in range(rows)]
+# matrix = np.chararray((rows, collumns), itemsize=15)
+# matrix[:] = " " #initialize
 
 ternary, non_ternary = grammar()
-
-for j in range(collumns):
+validcoords = []
+for j in range(0, collumns):
     for i in range(rows):
         if j > i:
             if i+1 == j:
-                matrix[i, j] = check_grammar("first", words[i], ternary, non_ternary)#check_grammar(words[i]) #first match
+                matrix[i][j] = check_grammar("first", words[i], ternary, non_ternary)#check_grammar(words[i]) #first match
                 #check_grammar(words[0])
-            else:
-                for k in range(1, j):
-                    con = matrix[i, k] + " " + matrix[k, j-1] #concatenation of the two states
-                    matrix[i, j] += check_grammar("second", con, ternary, non_ternary)
-print matrix
+        else:
+            matrix[i][j] = "X"
+
+concat = []
+for j in range(1, collumns):
+    for i in range(rows, -1, -1):
+        if j > i+1:
+            concat = ""
+            #check left
+            for k in range(1, j):
+                if matrix[i][k] != "X" and matrix[k][j] != "X":
+                    concat = matrix[i][k] + "," + matrix[k][j]
+                    if matrix[i][j] == check_grammar("second", concat, ternary, non_ternary):
+                        matrix[i][j] = check_grammar("second", concat, ternary, non_ternary)
+                    else:
+                        matrix[i][j] += check_grammar("second", concat, ternary, non_ternary)
+                    print k, matrix[i][j]
+for i in range(len(matrix)):
+    print matrix[i]
+print matrix[1][-1]
